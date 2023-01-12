862a076aa8067640b59bed1774fd23d4f13e4b85
Dave Lovemartin <1011822+davelovemartin@users.noreply.github.com>
2023-01-12 07:05:22 +0000

    adding integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx(), prefetch()]
+});
